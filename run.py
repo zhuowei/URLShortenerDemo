b0e8f7ed65aacc7abcdb5da025fc8abfabc6d9df
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-07-16 21:40:25 -0400

    Fixes for run.py, with a proper #! line and cgi-bin config

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python
 import CGIHTTPServer
 import SocketServer
 
 PORT = 8080
 
 Handler = CGIHTTPServer.CGIHTTPRequestHandler
-Handler.cgi_directories = ["c", "/c", "c/", "/c/"]
-print Handler.cgi_directories
+Handler.cgi_directories = ["/c"]
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print "Serving at port", PORT
+#change this if running on live website
 httpd.server_name = "localhost"
 httpd.server_port = PORT
 
